2d90ae5a17fbe9c5498b31bfe44b305bdf899f89
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-03-10 22:33:01 -0600

    photo controller index and create routes

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::PhotosController < ApplicationController
+
+    def index
+        photos = Photo.all
+        render json: photos
+    end
+
+    def create
+        photo = Photo.new(photo_params)
+        if photo.save
+            render json: photo, status: :created
+        else
+            render json: { errors: photos.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def photo_params
+        params.require(:photo).permit(:sol, :status, :img_src, :earth_date, :earthdate_id)
+    end
+
+
 end
