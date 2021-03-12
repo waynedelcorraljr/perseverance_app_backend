f0b1a12706496fc7e35a9b8fea3e1e5c3bf214ea
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-03-12 14:59:06 -0600

    further optimizes earthdate fetch, removes photos from earthdate serializer

--- app/controllers/api/v1/earthdates_controller.rb
+++ app/controllers/api/v1/earthdates_controller.rb
@@ -1,14 +1,18 @@
 class Api::V1::EarthdatesController < ApplicationController
 
     def index
-        self.create_earthdates_from_api
+        nasa = self.call_nasa
+        self.create_earthdates_from_api(nasa)
         earthdates = Earthdate.all
         render json: EarthdateSerializer.new(earthdates)
     end
 
-    def create_earthdates_from_api
+    def call_nasa
         response = HTTParty.get('https://api.nasa.gov/mars-photos/api/v1/manifests/Perseverance/?api_key=DEMO_KEY')
         ed_json_object_arr = response["photo_manifest"]["photos"]
+    end
+
+    def create_earthdates_from_api(ed_json_object_arr)
         ed_json_object_arr.each do |obj|
             Earthdate.find_or_create_by(date: obj["earth_date"], total_photos: obj["total_photos"])
         end
--- app/serializers/earthdate_serializer.rb
+++ app/serializers/earthdate_serializer.rb
@@ -1,4 +1,4 @@
 class EarthdateSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :date, :total_photos, :photos
+  attributes :date, :total_photos
 end
