a017cfa84afaaeda4a5e072cd56ebc33409647fb
Wayne del Corral Jr <wayne@percy.ai>
2023-01-24 11:29:34 -0600

    improves logic in CheckForNewJob

--- app/jobs/check_for_new_job.rb
+++ app/jobs/check_for_new_job.rb
@@ -2,20 +2,20 @@ class CheckForNewJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    @earthdates ||= Earthdate.call_nasa_for_earthdates
-    total_days_on_api = @earthdates&.[]('photo_manifest')&.[]('photos').size
+    earthdates = Earthdate.call_nasa_for_earthdates
+    total_days_on_api = earthdates&.[]('photo_manifest')&.[]('photos')&.size
     total_days_in_db = Earthdate.all.size
-    last_photo_earthdate = Photo.all.last.earth_date.to_datetime
+    last_photo_earthdate = Photo.last.earth_date.to_datetime
 
-    if total_days_in_db < total_days_on_api && @earthdates
-      Earthdate.create_earthdates_from_api(@earthdates)
+    if total_days_on_api && total_days_in_db < total_days_on_api
+      Earthdate.create_earthdates_from_api(earthdates)
       starting_index = total_days_on_api - (total_days_on_api - total_days_in_db)
-      new_photos = call_nasa_for_photos(starting_index)
-      create_photos_from_api(new_photos)
+      new_photos = Photo.call_nasa_for_photos(starting_index)
+      Photo.create_photos_from_api(new_photos)
     elsif last_photo_earthdate && last_photo_earthdate < Earthdate.last.date.to_datetime
       starting_index = Earthdate.all.pluck(:date).index(last_photo_earthdate)
-      new_photos = call_nasa_for_photos(starting_index)
-      create_photos_from_api(new_photos)
+      new_photos = Photo.call_nasa_for_photos(starting_index)
+      Photo.create_photos_from_api(new_photos)
     end
   end
 end
